c31be7d17024323fd67e674b83df74985e1923f9
Nfolio <139557367+Nfolio567@users.noreply.github.com>
2025-08-03 11:46:37 +0900

    update Monocon/README

--- lib/Monocon/README.md
+++ lib/Monocon/README.md
@@ -11,9 +11,128 @@
 ## インスタンス化
 
 ```cpp
-Actuator actuator(clockPin, buzzerPin, segmentPin, segmentVcc, motorPin, stMotor);
+Actuator actuator(int clockPin, int buzzerPin, int segmentPin[8], int segmentVcc[2], int motorPin[2], int stMotor[4]);
 ```
 
 ## 各引数の説明
-- clockPin: 出力基板のフリップフロップIC[^1]に入れるクロック。
-- buzzerPin: 
+
+- clockPin: 出力基板のフリップフロップIC [^1] に入れるクロック。
+- buzzerPin: ブザーのピン
+- segmentPin: ７セグメントLEDのピン
+- segmentVcc: 7セグメントLEDのVCC
+- motorPin: モーターのピン
+- stMotor: ステッピングモーターのピン
+
+[^1]: Dフリップフロップ、よくわからない人は<a href="https://analogista.jp/flipflop/#:~:text=%E3%81%8B%E3%82%8A%E3%81%BE%E3%81%99%E3%80%82-,D%E3%83%95%E3%83%AA%E3%83%83%E3%83%97%E3%83%95%E3%83%AD%E3%83%83%E3%83%97(D%2DFF),-D%E3%83%95%E3%83%AA%E3%83%83%E3%83%97%E3%83%95%E3%83%AD%E3%83%83%E3%83%97%E3%81%AF" target="_blank">このサイト</a>がオヌヌメ
+
+## 用意されてる定数
+
+- Actuator::Side
+  
+  LEFT or RIGHT
+
+- Actuator::Content
+  
+  Number: Num0, Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9
+  Alphabet: A, b, C, d, E, F, G, H, K, L, M, n, r, S, W
+  No content: None
+  
+  
+
+## 各メソッドの説明
+
+> ### actuator.segment(Side side, Content content, bool displayDots = false)
+
+#### 引数
+
+- side: 左右どっちが光るか（Actuator::Side::LEFT, Actuator::Side::RIGHT)
+
+- content: 光らせる文字、数字（Actuator::Content::Num0, Actuator::Content::H etc...)
+
+- displayDots: 7セグメントLEDの右下の点を表示するか否か、何も指定しなければfalse(表示しない)になる
+
+#### 説明
+
+制御基板にある7セグメントLEDを光らせるメソッド
+
+
+
+> ### actuator.segmentStop()
+
+#### 引数
+
+なし
+
+#### 説明
+
+7セグメントLEDを全て消灯させて、VCCもLOWにするメソッド
+
+
+
+> ### actuator.motor(Side side, int pmw, unsigned long ms)
+
+#### 引数
+
+- side: 左右どっち回すか
+
+- pmw: 回す速度（0~255）
+
+- ms: 回す時間（ミリ秒単位で指定）
+
+#### 説明
+
+制御基板にあるDCモーターを指定した速度、時間で回すメソッド
+
+
+
+> ### actuator.motorStop()
+
+#### 引数
+
+なし
+
+#### 説明
+
+モーターピンと繋がっているモータドライバの入力ピン両方にHIGHを入力し、モーターを停止状態にさせるメソッド
+
+
+
+> ### actuator.buzzer(int hertz, unsigned long ms)
+
+#### 引数
+
+- hertz: 周波数
+
+- ms: 鳴らす時間
+
+#### 説明
+
+※非推奨[^2]
+
+制御基板にあるブザーを指定時間鳴らすメソッド
+
+
+
+> ### actuator.steppingMotor(Side side, int step)
+
+#### 引数
+
+- side: 左右どっちに回すか
+
+- step: どのステップを入力するか（1~4）[^3]
+
+#### 説明
+
+制御基板にあるステッピングモーターを指定した方向に回すメソッド
+
+
+
+## 注釈
+
+[^1]: Dフリップフロップ、よくわからない人は<a href="https://analogista.jp/flipflop/#:~:text=%E3%81%8B%E3%82%8A%E3%81%BE%E3%81%99%E3%80%82-,D%E3%83%95%E3%83%AA%E3%83%83%E3%83%97%E3%83%95%E3%83%AD%E3%83%83%E3%83%97(D%2DFF),-D%E3%83%95%E3%83%AA%E3%83%83%E3%83%97%E3%83%95%E3%83%AD%E3%83%83%E3%83%97%E3%81%AF" target="_blank">このサイト</a>がオヌヌメ
+
+[^2]: toneメソッドを使ったほうが柔軟性あるから
+本番で一回も使わなかったのであまり必要性を感じていない
+
+[^3]: （前提として、制御基板にあるステッピングモーターは４ステップ区切りの一周120ステップ）回すステップ数ではなく、４ステップのうちどのステップを入力するか
+この方が柔軟性が高くて良い
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ using Content = Actuator::Content;
 constexpr int motor[] = {44, 45};
 constexpr int stMotor[4] = {40, 41, 42, 43};
 constexpr int clk = 29;
-constexpr int nasg[] = {40, 41, 42, 43, 44, 45, 46, 47};
-constexpr int nasgvcc[] = {24,25};
+constexpr int segment[] = {40, 41, 42, 43, 44, 45, 46, 47};
+constexpr int segmentVcc[] = {24,25};
 constexpr int buzzer = 28;
 constexpr int taktSwitch = A1;
 constexpr int variableResistance = A0;
@@ -16,13 +16,13 @@ int step = 0;
 int buzzerState = 0;
 unsigned long prevMillis = millis();
 unsigned long nowMillis = 0;
-Actuator actuator(clk, buzzer, nasg, nasgvcc, motor, stMotor);
+Actuator actuator(clk, buzzer, segment, segmentVcc, motor, stMotor);
 
 void setup() {
-  for (int i : motor) pinMode(i, 1);
-  for (int i : nasg) pinMode(i, 1);
-  for (int i : nasgvcc) pinMode(i, 1);
-  for (int i : stMotor) pinMode(i, 1);
+  for (const int i : motor) pinMode(i, 1);
+  for (const int i : segment) pinMode(i, 1);
+  for (const int i : segmentVcc) pinMode(i, 1);
+  for (const int i : stMotor) pinMode(i, 1);
   pinMode(clk, 1);
   pinMode(buzzer, 1);
   pinMode(taktSwitch, 0);
@@ -31,57 +31,11 @@ void setup() {
 }
 
 void loop() {
-  const int diode = analogRead(A2);
-  const int resiState = analogRead(variableResistance);
-  Serial.println(diode);
+  /** 暇やったらここにあるコード解読してみて
+   * もしかしたらコード不十分の可能性あるかも
+   */
 
-  if (resiState <= maxVRC && resiState >= minVRC) {
-    actuator.segment(Side::LEFT, Content::M);
-    delay(1);
-    actuator.segment(Side::RIGHT, Content::Num0);
-    delay(1);
-    if (buzzerState < 2) {
-      nowMillis = millis();
-      if (nowMillis - prevMillis >= 1000) {
-        noTone(buzzer);
-      } else if (nowMillis - prevMillis >= 0) {
-        tone(buzzer, 140);
-      }
-
-      if (nowMillis - prevMillis >= 2000) {
-        prevMillis = millis();
-        buzzerState++;
-      }
-    }
-    //Serial.println(nowMillis - prevMillis);
-  } else if (resiState <= maxVRL && resiState >= minVRL) {
-    prevMillis = millis();
-    noTone(buzzer);
-    buzzerState = 0;
-    actuator.segmentStop();
-    actuator.steppingMotor(Side::LEFT, step);
-    step++;
-    if (step == 4) step = 0;
-      actuator.segment(Side::LEFT, Content::L);
-    delay(1);
-    if (resiState <= maxVRL / 8) {
-      actuator.segment(Side::RIGHT, Content::Num8);
-      delay(1);
-    } else if (resiState <= maxVRL / 8 * 2) {
-      actuator.segment(Side::RIGHT, Content::Num7);
-      delay(1);
-    } else if (resiState <= maxVRL / 8 * 3) {
-      actuator.segment(Side::RIGHT, Content::Num6);
-      delay(1);
-    }
-  } else if (resiState <= maxVRR && resiState >= minVRR) {
-    prevMillis = millis();
-    noTone(buzzer);
-    buzzerState = 0;
-    actuator.segmentStop();
-    //actuator.motor()
-  }
-
+  // １個目
   /*const int switchState = digitalRead(taktSwitch);
   if (switchState == 0) {
     prevMillis = millis();
@@ -110,6 +64,7 @@ void loop() {
     Serial.println(nowMillis - prevMillis);
   }*/
 
+  // ２個目
   /*if (switchState == 1) {
     actuator.segmentStop();
     actuator.motorStop();
@@ -123,6 +78,7 @@ void loop() {
     delay(1);
   }*/
 
+  // ３個目
   /*step = 0;
   for (int i=0;i<240;i++) {  
     actuator.segmentStop();
@@ -143,6 +99,7 @@ void loop() {
     delay(1);
   }*/
 
+  // ４個目
   /*actuator.segmentStop();
   actuator.steppingMotor(Side::RIGHT, step);
   step++;
@@ -155,9 +112,4 @@ void loop() {
   if(step == 4) step=0;
   actuator.segment(Side::RIGHT, Content::d); 
   delay(1);*/
-
-  /*for (int i=0;i<120;i++) {
-    actuator.steppingMotor(Actuator::RIGHT, step);
-  }
-  delay(1000);*/
 }
